5329c1c2901ce73f2ead67f4a91e80b1652e2355
Aleksey Stepanov <aleksey.stepanov@dualbootpartners.com>
2021-11-18 13:54:03 +0400

    [#1286] added login and test for it

--- lib/uffizzi/cli.rb
+++ lib/uffizzi/cli.rb
@@ -1,12 +1,25 @@
 # frozen_string_literal: true
 
 require 'thor'
+require 'io/console'
 
 module Uffizzi
   class CLI < Thor
     desc 'version', 'show version'
     def version
       puts Uffizzi::VERSION
+    end
+
+    desc 'login', 'login'
+    method_option :user, :required => true, :aliases => '-u'
+    method_option :hostname, :required => true, :aliases => '-h'
+    def login
+      password = IO::console.getpass "Enter Password: "
+      {
+        user: options[:user],
+        password: password.strip,
+        hostname: options[:hostname]
+      }
     end
   end
 end
--- test/uffizzi_test.rb
+++ test/uffizzi_test.rb
@@ -1,9 +1,25 @@
 # frozen_string_literal: true
 
 require 'test_helper'
+require 'uffizzi/cli'
 
 class UffizziTest < Minitest::Test
+
   def test_that_it_has_a_version_number
     refute_nil(::Uffizzi::VERSION)
+  end
+
+  def test_login
+    cli = Uffizzi::CLI.new
+    command_params = {
+      user: 'test@email.com',
+      password: 'test_pass',
+      hostname: 'localhost'
+    }
+    cli.options = { user: command_params[:user], hostname: command_params[:hostname] }
+    IO::console.stub(:getpass, command_params[:password]) do
+      responce = cli.login
+      assert_equal responce, command_params
+    end
   end
 end
