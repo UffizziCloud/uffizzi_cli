5dd970b6977128fe182bf2a34369005127b37832
Aleksey Stepanov <aleksey.stepanov@dualbootpartners.com>
2021-11-26 16:01:20 +0400

    changed naming

--- lib/uffizzi.rb
+++ lib/uffizzi.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
 require_relative 'uffizzi/version'
-require_relative 'uffizzi/client/api/api_client'
-require_relative 'uffizzi/client/api/api_response'
-require_relative 'uffizzi/client/api/api_routes'
+require_relative 'uffizzi/clients/api/api_client'
+require_relative 'uffizzi/clients/api/api_response'
+require_relative 'uffizzi/clients/api/api_routes'
 require_relative 'uffizzi/config'
 
 module Uffizzi
