3eb6955f6dd6c02398c05cb18290d628492d84db
Aleksey Stepanov <aleksey.stepanov@dualbootpartners.com>
2021-12-06 14:31:20 +0400

    improved errors printing and renamed app config

--- lib/uffizzi/clients/api/api_client.rb
+++ lib/uffizzi/clients/api/api_client.rb
@@ -21,12 +21,7 @@ module ApiClient
 
 
   def self.print_errors(errors)
-    all_errors_messages = []
-    errors.each do |error|
-      _field_name, *error_messages = error
-      all_errors_messages.push(*error_messages)
-    end
-    all_errors_messages.each { |error_message| puts error_message }
+    puts errors.keys.reduce([]) { |acc, key| acc.push(errors[key]) }
   end
 
   private
--- test/test_setup_helper.rb
+++ test/test_setup_helper.rb
@@ -1,5 +1,5 @@
 require 'minitest/autorun'
-require_relative '../config/config'
+require_relative '../config/uffizzi'
 
 class Minitest::Test
 
