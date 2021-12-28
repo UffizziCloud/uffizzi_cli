bb169f7ad45a9e880d416621722892f94132d905
Aleksey Stepanov <aleksey.stepanov@dualbootpartners.com>
2021-12-28 16:28:53 +0400

    fixed basic auth for https, get requests with params and rewriting config after login

--- lib/uffizzi/config_file.rb
+++ lib/uffizzi/config_file.rb
@@ -9,7 +9,8 @@ module Uffizzi
 
     class << self
       def create(account_id, cookie, hostname)
-        write(prepare_config_data(account_id, cookie, hostname))
+        data = prepare_config_data(account_id, cookie, hostname)
+        data.each_pair { |key, value| write_option(key, value) }
       end
 
       def delete
@@ -36,7 +37,9 @@ module Uffizzi
       end
 
       def write_option(key, value)
-        data = read
+        data = exists? ? read : {}
+        return nil if data.nil?
+
         data[key] = value
         write(data.to_json)
       end
@@ -80,8 +83,6 @@ module Uffizzi
           hostname: hostname,
           cookie: cookie,
         }
-
-        data.to_json
       end
 
       def create_file
