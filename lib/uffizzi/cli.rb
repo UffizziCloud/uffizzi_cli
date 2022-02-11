78e414090e671ee38db6b9809127c1ed2e6bc9a8
kitsunegakkou@gmail.com <lidia.mokevnina@dualbootpartners.com>
2022-02-11 15:45:42 +0300

    added help option

--- lib/uffizzi/cli.rb
+++ lib/uffizzi/cli.rb
@@ -1,12 +1,14 @@
 # frozen_string_literal: true
 
 require 'thor'
+require 'byebug'
 
 module Uffizzi
   class CLI < Thor
     require_relative 'cli/common'
 
-    class_option :help, type: :boolean, aliases: '-h'
+    class_option :help, type: :boolean, aliases: ['-h', 'help']
+    argument :help, type: :string, required: false
 
     desc 'version', 'Show Version'
     def version
@@ -23,7 +25,7 @@ module Uffizzi
 
     desc 'logout', 'Logout from Uffizzi'
     def logout
-      return Cli::Common.show_manual(:logout) if options[:help]
+      return Cli::Common.show_manual(:logout) if has_help_option?(options)
 
       require_relative 'cli/logout'
       Logout.new.run
@@ -39,6 +41,12 @@ module Uffizzi
     def config(command, property = nil, value = nil)
       require_relative 'cli/config'
       Config.new.run(command, property, value)
+    end
+
+    private
+
+    def has_help_option?(options)
+      options[:help] || ARGV.last == 'help'
     end
   end
 end
