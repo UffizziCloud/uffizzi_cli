44fda2f74d6f0b39f5124b9a97dd353ae4ab34e9
Aleksey Stepanov <aleksey.stepanov@dualbootpartners.com>
2022-02-16 19:29:06 +0400

    tests fixed

--- test/uffizzi/cli/config_test.rb
+++ test/uffizzi/cli/config_test.rb
@@ -5,48 +5,30 @@ require 'uffizzi'
 
 class ConfigTest < Minitest::Test
   def setup
-    @cli = Uffizzi::CLI.new
+    @config = Uffizzi::CLI::Config.new
 
     sign_in
   end
 
   def test_list
-    result = @cli.config('list')
+    result = @config.list
 
     assert_equal(result[:account_id], @account_id)
     assert_equal(result[:cookie], @cookie)
     assert_equal(result[:hostname], Uffizzi.configuration.hostname)
   end
 
-  def test_get_without_property
-    @cli.config('get')
-
-    assert_equal('No property provided', Uffizzi.ui.last_message)
-  end
-
   def test_get_with_property
-    @cli.config('get', 'cookie')
+    @config.get('cookie')
 
     assert_equal(Uffizzi::ConfigFile.read_option(:cookie), Uffizzi.ui.last_message)
   end
 
   def test_get_with_wrong_property
     unexisted_property = 'unexisted_property'
-    @cli.config('get', unexisted_property)
+    @config.get(unexisted_property)
 
     assert_equal("The option #{unexisted_property} doesn't exist in config file", Uffizzi.ui.last_message)
-  end
-
-  def test_set_without_property_and_value
-    @cli.config('set')
-
-    assert_equal("No property provided\nNo value provided", Uffizzi.ui.last_message)
-  end
-
-  def test_set_without_value
-    @cli.config('set', 'cookie')
-
-    assert_equal('No value provided', Uffizzi.ui.last_message)
   end
 
   def test_set_with_property_and_value
@@ -54,7 +36,7 @@ class ConfigTest < Minitest::Test
 
     refute_equal(Uffizzi::ConfigFile.read_option(:cookie), new_cookie)
 
-    @cli.config('set', 'cookie', new_cookie)
+    @config.set('cookie', new_cookie)
 
     assert_equal(Uffizzi::ConfigFile.read_option(:cookie), new_cookie)
   end
@@ -64,19 +46,13 @@ class ConfigTest < Minitest::Test
 
     Uffizzi::ConfigFile.delete
 
-    @cli.config('set', 'cookie', cookie)
+    @config.set('cookie', cookie)
 
     assert_equal(cookie, Uffizzi::ConfigFile.read_option(:cookie))
   end
 
-  def test_delete_without_property
-    @cli.config('delete')
-
-    assert_equal('No property provided', Uffizzi.ui.last_message)
-  end
-
   def test_delete_with_property
-    @cli.config('delete', 'cookie')
+    @config.delete('cookie')
 
     refute(Uffizzi::ConfigFile.read_option(:cookie))
   end
