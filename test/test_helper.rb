0cae65ad100da2d99bb917a54f32d9f20189727b
Anton Chepenko <chepenkoanton2011@yandex.ru>
2022-02-22 14:42:04 +0400

    added require io/console

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,7 +5,6 @@ Dir[File.expand_path('support/**/*.rb', __dir__)].sort.each { |f| require f }
 
 require 'factory_bot'
 require 'net/http'
-require 'io/console'
 require 'byebug'
 require 'minitest/autorun'
 require 'webmock/minitest'
--- test/uffizzi/cli/login_test.rb
+++ test/uffizzi/cli/login_test.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require 'test_helper'
+require 'io/console'
 
 class LoginTest < Minitest::Test
   def setup
