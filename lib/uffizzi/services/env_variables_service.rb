795848a2b97e76f41a602e8eead02821ffac2e6a
Aleksey Stepanov <aleksey.stepanov@dualbootpartners.com>
2022-03-14 13:54:38 +0400

    env variables substituting moved to separate service

---
+++ lib/uffizzi/services/env_variables_service.rb
@@ -0,0 +1,48 @@
+# frozen_string_literal: true
+
+require 'psych'
+require 'pathname'
+require 'base64'
+
+class EnvVariablesService
+  class << self
+    def substitute_env_variables(compose_file_data)
+      compose_file_data.gsub(/\$\{?([?:\-_A-Za-z0-9]+)\}?/) do |variable|
+        variable_content = variable.match(/[?:\-_A-Za-z0-9]+/).to_s
+        fetch_variable_value(variable_content)
+      end
+    end
+
+    private
+
+    def fetch_variable_value(variable_content)
+      variable_name = variable_content.match(/^[_A-Za-z0-9]+/).to_s
+      variable_value = ENV[variable_name]
+      return variable_value unless variable_value.nil?
+      return fetch_variable_default_value(variable_content) if variable_has_default_value?(variable_content)
+
+      error_message = if variable_has_error_message?(variable_content)
+        fetch_env_error_message(variable_content)
+      else
+        "Environment variable #{variable_name} doesn't exist"
+      end
+      raise StandardError.new(error_message)
+    end
+
+    def variable_has_default_value?(variable_content)
+      variable_content.include?('-')
+    end
+
+    def fetch_variable_default_value(variable_content)
+      variable_content.split('-', 2).last
+    end
+
+    def variable_has_error_message?(variable_content)
+      variable_content.include?('?')
+    end
+
+    def fetch_env_error_message(variable_content)
+      variable_content.split('?', 2).last
+    end
+  end
+end
