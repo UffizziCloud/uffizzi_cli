70f5b0f0ec9fdb579bf0b08d780261a4ee7d53f5
Aleksey Stepanov <step_al_ul@mail.ru>
2022-04-01 17:20:56 +0400

    added ooutput option for previre create command

--- lib/uffizzi/shell.rb
+++ lib/uffizzi/shell.rb
@@ -5,6 +5,8 @@ require 'thor'
 module Uffizzi
   module UI
     class Shell
+      attr_accessor :output_format
+
       def initialize
         @shell = Thor::Shell::Basic.new
       end
@@ -27,6 +29,25 @@ module Uffizzi
 
       def last_message
         @shell.send(:stdout).string.strip
+      end
+
+      def disable_stdout
+        $stdout = StringIO.new
+      end
+
+      def output(outputed_object)
+        $stdout = IO.new(1, 'w')
+        output_format == 'json' ? output_in_json(outputed_object) : output_in_github_format(outputed_object)
+      end
+
+      def output_in_json(outputed_object)
+        say(outputed_object.to_json)
+      end
+
+      def output_in_github_format(outputed_object)
+        outputed_object.each_key do |key|
+          say("::set-output name=#{key}::#{outputed_object[key]}")
+        end
       end
     end
   end
