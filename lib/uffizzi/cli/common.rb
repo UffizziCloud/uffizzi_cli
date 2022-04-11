301b52cd4955edd713a13e2d1c8024e155a1c83e
Anton Chepenko <chepenkoanton2011@yandex.ru>
2022-04-11 20:02:35 +0400

    [#55] added root path to man command

--- lib/uffizzi/cli/common.rb
+++ lib/uffizzi/cli/common.rb
@@ -6,7 +6,7 @@ module Cli
   class Common
     class << self
       def show_manual(command_name)
-        manual_doc_path = "man/#{command_name}"
+        manual_doc_path = File.join(Uffizzi.root, "man/#{command_name}")
 
         Open3.pipeline("man #{manual_doc_path}")
       end
