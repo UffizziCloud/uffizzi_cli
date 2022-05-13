97c9473eef83a6d9d2d730ee464be55f054c09c8
Anton Chepenko <chepenkoanton2011@yandex.ru>
2022-05-13 16:27:30 +0400

    fixed api client

--- lib/uffizzi/clients/api/api_client.rb
+++ lib/uffizzi/clients/api/api_client.rb
@@ -30,14 +30,14 @@ module ApiClient
   def fetch_credentials(server)
     uri = credentials_uri(server)
 
-    response = Uffizzi::HttpClient.make_get_request(uri)
+    response = http_client.make_get_request(uri)
 
     build_response(response)
   end
 
   def check_credential(server, type)
     uri = check_credential_uri(server, type)
-    response = Uffizzi::HttpClient.make_get_request(uri)
+    response = http_client.make_get_request(uri)
 
     build_response(response)
   end
