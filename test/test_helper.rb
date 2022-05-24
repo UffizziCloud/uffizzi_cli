68a1c8b2668e4a74c9d1325c65020edb7a35068d
Aleksey Stepanov <step_al_ul@mail.ru>
2022-05-24 12:51:43 +0400

    [#94] created auth support and removed sign_in and sign_out from test_helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -18,6 +18,7 @@ require 'uffizzi/cli'
 require 'uffizzi/config_file'
 require 'uffizzi/shell'
 
+include AuthSupport
 include FixtureSupport
 include UffizziStubSupport
 include UffizziComposeStubSupport
@@ -43,30 +44,5 @@ class Minitest::Test
     super
 
     Uffizzi::ConfigFile.delete
-  end
-
-  def sign_in
-    @cookie = '_uffizzi=test'
-    login_body = json_fixture('files/uffizzi/uffizzi_login_success.json')
-    @account_id = login_body[:user][:accounts].first[:id].to_s
-    data = prepare_config_data
-    data.each_pair { |key, value| Uffizzi::ConfigFile.write_option(key, value) }
-  end
-
-  def prepare_config_data
-    {
-      account_id: @account_id,
-      server: Uffizzi.configuration.server,
-      cookie: @cookie,
-    }
-  end
-
-  def command_options(options)
-    Thor::CoreExt::HashWithIndifferentAccess.new(options)
-  end
-
-  def sign_out
-    Uffizzi::ConfigFile.unset_option(:cookie)
-    Uffizzi::ConfigFile.unset_option(:account_id)
   end
 end
