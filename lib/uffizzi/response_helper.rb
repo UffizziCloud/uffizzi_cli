4d4a666cb3559ccb7534ed1cbbb4c7775e41a81f
Anton Lubin <anton.lubin@dualbootpartners.com>
2022-07-26 20:53:56 +0500

    [#130] Prevent error duplication

--- lib/uffizzi/response_helper.rb
+++ lib/uffizzi/response_helper.rb
@@ -42,7 +42,7 @@ module Uffizzi
       def prepare_errors(errors)
         errors.values.reduce('') do |acc, error_messages|
           if error_messages.is_a?(Array)
-            error_messages.each { |error_message| acc = "#{acc}#{prepare_error_message(error_message)}\n" }
+            error_messages.uniq.each { |error_message| acc = "#{acc}#{prepare_error_message(error_message)}\n" }
           else
             acc = "#{acc}#{prepare_error_message(error_message)}\n"
           end
