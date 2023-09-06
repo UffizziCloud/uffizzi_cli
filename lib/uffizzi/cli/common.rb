01027f51b56aa09c4d064e8476c7d810ae666577
Lidia Mokevnina <kitsunegakkou@gmail.com>
2023-09-06 11:36:01 +0200

    [478] fixed gha job

--- lib/uffizzi/cli/common.rb
+++ lib/uffizzi/cli/common.rb
@@ -4,9 +4,7 @@ module Uffizzi
   class Cli::Common
     class << self
       def show_manual(command_name)
-        puts "show manual start"
         manual_doc_path = File.join(Uffizzi.root, "man/#{command_name}.ronn")
-        puts "found manual_doc_path"
 
         Uffizzi.ui.say(File.read(manual_doc_path))
       end
